f09123a2e18b7a26e35707538d34e4ea9f194840
Louys <louysdev@gmail.com>
2022-09-27 10:44:52 -0400

    Creando diferentes archivos y variables

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,26 @@
-const { src, dest } = require("gulp")
+const { src, dest, watch } = require("gulp")
 const sass = require('gulp-sass')(require('sass'))
 
-sass.compiler = require("dart-sass");
+sass.compiler = require("dart-sass")
 
-function css() {
+function css(done) {
     return src("./src/scss/app.scss")
         .pipe(sass())
         .pipe(dest("./build/css"))
 }
 
+function minificarcss() {
+    return src("./src/scss/app.scss")
+    .pipe(sass({
+        outputStyle: "compressed"
+    }))
+    .pipe(dest("./build/css"))
+}
+
+function whatchArchivos() {
+    watch("src/scss/**/*.scss", css)
+}
+
 exports.css = css
+exports.minificarcss = minificarcss
+exports.whatchArchivos = whatchArchivos
