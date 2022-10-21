5bf906fbbf12d379481eae1b39b4fecbdc21daf0
Louys <louysdev@gmail.com>
2022-10-21 17:19:32 -0400

    Detectar el Scroll con JS

--- build/js/bundle.js
+++ build/js/bundle.js
@@ -1,6 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
     scrollNav();
+    navegacionFija();
 })
+
+function navegacionFija() {
+    const barra = document.querySelector(".header");
+    const sobreFestival = document.querySelector(".sobre-festival");
+
+    window.addEventListener("scroll", function () {
+        if (sobreFestival.getBoundingClientRect().bottom < 0) {
+            barra.classList.add("fijo");
+        } else {
+            barra.classList.remove("fijo");
+        }
+    })
+}
 
 function scrollNav() {
     const enlaces = document.querySelectorAll(".navegacion-principal a");
